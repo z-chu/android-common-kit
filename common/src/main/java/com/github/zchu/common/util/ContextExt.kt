b77248c9dfc32e574adc6e7cac155ba1a1d620a6
zchu <zchu8073@gmail.com>
2019-04-04 14:15:06 +0800

    Add Context Extensions

--- common/src/main/java/com/github/zchu/common/util/ContextExt.kt
+++ common/src/main/java/com/github/zchu/common/util/ContextExt.kt
@@ -2,10 +2,18 @@ package com.github.zchu.common.util
 
 import android.content.Context
 import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
 import androidx.annotation.FontRes
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import java.util.concurrent.Executor
 
 fun Context.getColorCompat(@ColorRes colorRes: Int) = ContextCompat.getColor(this, colorRes)
 
+fun Context.getColorStateListCompat(@ColorRes colorRes: Int) = ContextCompat.getColorStateList(this, colorRes)
+
 fun Context.getFontCompat(@FontRes fontRes: Int) = ResourcesCompat.getFont(this, fontRes)
+
+fun Context.getDrawableCompat(@DrawableRes drawableRes: Int) = ContextCompat.getDrawable(this, drawableRes)
+
+fun Context.getMainExecutorCompat(): Executor = ContextCompat.getMainExecutor(this)
