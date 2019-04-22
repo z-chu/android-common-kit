ec67172a085a4b0ee16ed8f783a19fe5a8549f84
zchu <zchu8073@gmail.com>
2019-04-22 18:25:31 +0800

    Update ViewExt.kt

--- common/src/main/java/com/github/zchu/common/util/ViewExt.kt
+++ common/src/main/java/com/github/zchu/common/util/ViewExt.kt
@@ -1,8 +1,16 @@
 package com.github.zchu.common.util
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MotionEvent
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.annotation.IntRange
 
 
 fun View.removeSelfFromParent() {
@@ -59,3 +67,67 @@ fun <T : View> View.findViewByClass(clazz: Class<T>): T? {
     }
     return null
 }
+
+
+/** Listens for item selection in a Spinner, sending the position to a callback. */
+fun Spinner.onItemSelected(cb: (Int) -> Unit) {
+    onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onNothingSelected(parent: AdapterView<*>?) = Unit
+
+        override fun onItemSelected(
+            parent: AdapterView<*>?,
+            view: View?,
+            position: Int,
+            id: Long
+        ) = cb(position)
+    }
+}
+
+/** Listens for an EditText's text changes and sends them into a callback. */
+fun EditText.doOnTextChanged(
+    @IntRange(from = 0, to = 10000) debounce: Int = 0,
+    cb: (String) -> Unit
+) {
+    addTextChangedListener(object : TextWatcher {
+        val callbackRunner = Runnable {
+            cb(text.trim().toString())
+        }
+
+        override fun afterTextChanged(s: Editable?) = Unit
+
+        override fun beforeTextChanged(
+            s: CharSequence,
+            start: Int,
+            count: Int,
+            after: Int
+        ) = Unit
+
+        override fun onTextChanged(
+            s: CharSequence,
+            start: Int,
+            before: Int,
+            count: Int
+        ) {
+            removeCallbacks(callbackRunner)
+            if (debounce == 0) {
+                callbackRunner.run()
+            } else {
+                postDelayed(callbackRunner, debounce.toLong())
+            }
+        }
+    })
+}
+
+
+/** Sets a view's visible to [VISIBLE]. */
+fun View.show() {
+    visibility = VISIBLE
+}
+
+/** Sets a view's visible to [GONE]. */
+fun View.hide() {
+    visibility = GONE
+}
+
+/** If [show] is true, calls [show] on the receiving view, else [hide]. */
+fun View.showOrHide(show: Boolean) = if (show) show() else hide()
