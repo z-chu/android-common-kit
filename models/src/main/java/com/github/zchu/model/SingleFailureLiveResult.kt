1ab0a1c4b1b5952ad6266367a08d8d263ffd224d
zchu <zchu8073@gmail.com>
2019-06-13 15:28:25 +0800

    Update

--- models/src/main/java/com/github/zchu/model/SingleFailureLiveResult.kt
+++ models/src/main/java/com/github/zchu/model/SingleFailureLiveResult.kt
@@ -2,12 +2,13 @@ package com.github.zchu.model
 
 import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 
 /**
  * 如果最后一次结果是Failure，则在一个新的Observer订阅后，不会收到之前的Failure结果
  */
-class SingleFailureLiveResult<T> : LiveResult<T>() {
+class SingleFailureLiveResult<T> : MutableLiveData<WorkResult<T>>() {
 
 
     @MainThread
